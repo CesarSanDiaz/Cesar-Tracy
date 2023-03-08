ee5eb3d2c66725f061f089281736de373661bd53
cesarsanchezdiaz <sanchezdiaz104@gmail.com>
2023-03-08 07:05:40 -0800

    first commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Cesar & Tracy's Page
+Hi, this is a little glips into our life together through pictures, videos, and much more!
---
+++ script.js
@@ -0,0 +1,54 @@
+feather.replace();
+// scroll to about us using down button 
+const aboutUsSection = document.querySelector('.about-us');
+const downBtn = document.getElementById('down-btn');
+const leftBtn = document.getElementById('left-btn');
+const rightBtn = document.getElementById('right-btn');
+
+// adding a click event listener to my function
+downBtn.addEventListener('click', scrollToAboutUs);
+//my function
+function scrollToAboutUs() {
+  aboutUsSection.scrollIntoView(true);
+}
+
+// <<<<=====About us buttons====>>>>>>
+
+// array of images that I want to go through
+const images = [
+  "images/img_avatar.png",
+  "images/img_avatar2.png"
+]
+
+let currentImage = null;
+
+const currentImageElement = document.getElementById('active-image');
+const leftBtnElement = document.getElementById('left-btn');
+const rightBtnElement = document.getElementById('right-btn');
+
+const setActiveImage = (targetImage) => {
+  currentImage = targetImage;
+  currentImageElement.setAttribute('src', targetImage);
+}
+
+setActiveImage(images[0]);
+
+leftBtnElement.addEventListener('click', () => {
+  let currentIndex = images.indexOf(currentImage);
+  if (currentIndex - 1 < 0) {
+    currentIndex = images.length - 1;
+  } else {
+    currentIndex--;
+  }
+  setActiveImage(images[currentIndex]);
+})
+
+rightBtnElement.addEventListener('click', () => {
+  let currentIndex = images.indexOf(currentImage);
+  if (currentIndex + 1 === images.length) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+  setActiveImage(images[currentIndex]);
+})
